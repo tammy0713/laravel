38a0a5fcf5494f55eeb231bab01093d20f879692
Tammy <1379614760@qq.com>
2018-09-29 08:59:20 +0800

    2018-9-29-exam

---
+++ app/Http/Controllers/Hello/ExamController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tammy
+ * Date: 2018/9/29
+ * Time: 8:57
+ */
+
+namespace App\Http\Controllers\Hello;
+
+
+use App\Http\Controllers\Controller;
+
+class ExamController extends Controller
+{
+   public function index()
+   {
+       echo "1";
+   }
+}
